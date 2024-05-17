ef18b8a936d2be49f86d7a4642d30e0c70dcaf88
baophm03 <baoisme2003@gmail.com>
2024-05-17 18:08:11 +0700

    Complete basic features

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-import React, { useEffect } from 'react';
-import TrackPlayer from 'react-native-track-player'
-import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
-
-const tracks = [
-  {
-    id: 1,
-    url: require('./assets/Musics/noitinhyeubatdau.mp3'),
-    title: 'Nơi tình yêu bắt đầu',
-  },
-  {
-    id: 2,
-    url: require('./assets/Musics/noitinhyeubatdau.mp3'),
-    title: 'Blues Beat',
-  },
-];
+import React, { useEffect, useState } from 'react';
+import TrackPlayer, { State, Event, useTrackPlayerEvents, useProgress } from 'react-native-track-player';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import tracks from './tracks';
 
 TrackPlayer.updateOptions({
   stopWithApp: false,
@@ -24,7 +12,21 @@ TrackPlayer.updateOptions({
   ],
 });
 
+// Hàm chuyển đổi thời gian từ giây sang định dạng "hh:mm:ss"
+function secondsToMMSS(seconds) {
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 const MusicPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTrack, setCurrentTrack] = useState(null);
+
   const setUpTrackPlayer = async () => {
     try {
       await TrackPlayer.setupPlayer();
@@ -41,35 +43,70 @@ const MusicPlayer = () => {
     return () => TrackPlayer.destroy();
   }, []);
 
+  useTrackPlayerEvents([Event.PlaybackTrackChanged], async (event) => {
+    if (event.type === Event.PlaybackTrackChanged && event.nextTrack != null) {
+      const track = await TrackPlayer.getTrack(event.nextTrack);
+      setCurrentTrack(track);
+    }
+  });
+
+  const togglePlayPause = async () => {
+    const currentState = await TrackPlayer.getState();
+    if (currentState === State.Playing) {
+      await TrackPlayer.pause();
+      setIsPlaying(false);
+    } else {
+      await TrackPlayer.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const time = useProgress();
+
+  const time1 = roundNumber(time.position);
+  const time2 = roundNumber(time.duration);
+  function roundNumber(number) {
+    // Làm tròn số về phía trước nếu phần thập phân nhỏ hơn 0.5, ngược lại làm tròn về phía sau
+    return number % 1 < 0.5 ? Math.floor(number) : Math.ceil(number);
+  }
+
+
+  const thoigianbatdau = secondsToMMSS(time1);
+  const thoigianketthuc = secondsToMMSS(time2);
   return (
     <View style={styles.container}>
-
       {/* Thanh thông tin */}
       <View style={styles.topbar}>
-        <View style={{ alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{ fontSize: 15, fontWeight: 'bold' }}>Nhạc Nhẽo</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 17, fontWeight: 'bold', color: 'black' }}>Nhạc Nhẽo</Text>
         </View>
       </View>
 
       {/* Ảnh bìa bài hát */}
-      <Image style={styles.albumArt}
-        source={require('./assets/Images/minhyeunhaudi.jpg')}
-      />
+      {currentTrack && (
+        <Image style={styles.albumArt} source={currentTrack.artwork} />
+      )}
 
+      {/* Bar thông tin bài hát*/}
       <View style={styles.barthongtinbaihat}>
 
-        <TouchableOpacity style={{ padding: 15 }}>
+        <TouchableOpacity>
           <Image style={{ width: 20, height: 20 }} // Tùy chỉnh kích thước của icon
             source={require('./assets/Images/share.png')} // Đường dẫn đến icon trong thiết bị của bạn
           />
         </TouchableOpacity>
 
+        {/* Thông tin bài hát*/}
         <View style={styles.thongtinbaihat}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Mình Yêu Nhau Đi</Text>
-          <Text style={styles.artist}>Bích Phương</Text>
+          {currentTrack && (
+            <>
+              <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'black'}}>{currentTrack.title}</Text>
+              <Text style={styles.artist}>{currentTrack.artist}</Text>
+            </>
+          )}
         </View>
 
-        <TouchableOpacity style={{ padding: 15 }}>
+        <TouchableOpacity>
           <Image style={{ width: 20, height: 20 }} // Tùy chỉnh kích thước của icon
             source={require('./assets/Images/heart.png')} // Đường dẫn đến icon trong thiết bị của bạn
           />
@@ -78,47 +115,38 @@ const MusicPlayer = () => {
 
       {/* Khung hiển thị thời gian */}
       <View style={styles.khungthoigian}>
-        <View style={styles.thanhthoigian}>
-          <View style={[styles.thanhthoigianDo, { flex: 3 }]} />
-          <View style={[styles.thanhthoigianXanh, { flex: 1 }]} />
+      <View style={styles.thanhthoigian}>
+          <View style={[styles.thanhthoigianDo, { width: `${(time1 / time2) * 100}%` }]} />
+          <View style={[styles.thanhthoigianXanh, { width: `${((time2 - time1) / time2) * 100}%` }]} />
         </View>
 
         <View style={styles.thanhthoigian2}>
-          <Text style={styles.thoigianbatdau}>0:03</Text>
-          <Text style={styles.thoigianketthuc}>3:23</Text>
+          <Text style={styles.thoigianbatdau}>{(thoigianbatdau)}</Text>
+          <Text style={styles.thoigianketthuc}>{(thoigianketthuc)}</Text>
         </View>
       </View>
 
       {/* Thanh chức năng */}
       <View style={styles.thanhchucnang}>
-        <TouchableOpacity style={{ padding: 15 }}
+        <TouchableOpacity style={{ padding: 20 }}
           onPress={() => TrackPlayer.skipToPrevious()}>
           <Image style={{ width: 30, height: 30 }} // Tùy chỉnh kích thước của icon
             source={require('./assets/Images/Previous.png')} // Đường dẫn đến icon trong thiết bị của bạn
           />
         </TouchableOpacity>
 
-        <TouchableOpacity style={{ padding: 15 }}
-          onPress={() => TrackPlayer.play()}>
-          <Image style={{ width: 30, height: 30 }} // Tùy chỉnh kích thước của icon
-            source={require('./assets/Images/play.png')} // Đường dẫn đến icon trong thiết bị của bạn
+        <TouchableOpacity style={{ padding: 30 }} onPress={togglePlayPause}>
+          <Image style={{ width: 60, height: 60 }} // Tùy chỉnh kích thước của icon
+            source={isPlaying ? require('./assets/Images/pause.png') : require('./assets/Images/play.png')} // Đường dẫn đến icon trong thiết bị của bạn
           />
         </TouchableOpacity>
 
-        <TouchableOpacity style={{ padding: 15 }}
-          onPress={() => TrackPlayer.pause()}>
-          <Image style={{ width: 30, height: 30 }} // Tùy chỉnh kích thước của icon
-            source={require('./assets/Images/pause.png')} // Đường dẫn đến icon trong thiết bị của bạn
-          />
-        </TouchableOpacity>
-
-        <TouchableOpacity style={{ padding: 15 }}
+        <TouchableOpacity style={{ padding: 20 }}
           onPress={() => TrackPlayer.skipToNext()}>
           <Image style={{ width: 30, height: 30 }} // Tùy chỉnh kích thước của icon
             source={require('./assets/Images/next.png')} // Đường dẫn đến icon trong thiết bị của bạn
           />
         </TouchableOpacity>
-
       </View>
     </View>
   );
@@ -129,21 +157,30 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: 'white',
+    backgroundColor: 'pink',
   },
-
+  topbar: {
+    paddingTop: 10,
+    paddingBottom: 40,
+  },
   albumArt: {
     alignItems: 'center',
     margin: 30,
     borderRadius: 200,
-    width: 300,
-    height: 300,
+    width: 330,
+    height: 330,
   },
   barthongtinbaihat: {
+    paddingTop: 40,
+    paddingBottom: 40,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    width: '90%',
+    alignItems: 'center',
   },
   thongtinbaihat: {
     alignItems: 'center',
+    textAlign: 'center',
     width: 230,
   },
   khungthoigian: {
@@ -175,6 +212,8 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   thanhchucnang: {
+    paddingTop: 30,
+    paddingBottom: 40,
     flexDirection: 'row',
     alignItems: 'center',
   },
---
+++ tracks.js
@@ -0,0 +1,64 @@
+const tracks = [
+  {
+    url: require('./assets/Musics/noitinhyeubatdau/noitinhyeubatdau.mp3'),
+    title: 'Nơi tình yêu bắt đầu',
+    artist: 'Bùi Anh Tuấn',
+    artwork: require('./assets/Musics/noitinhyeubatdau/noitinhyeubatdau.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/tudo/tudo.mp3'),
+    title: 'Từ đó - OST Mắt Biếc',
+    artist: 'Phan Mạnh Quỳnh',
+    artwork: require('./assets/Musics/tudo/tudo.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/cochangtraivietlencay/cochangtraivietlencay.mp3'),
+    title: 'Có chàng trai viết lên cây',
+    artist: 'Phan Mạnh Quỳnh',
+    artwork: require('./assets/Musics/cochangtraivietlencay/cochangtraivietlencay.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/phocuconanh/phocuconanh.mp3'),
+    title: 'Phố cũ còn anh',
+    artist: 'Quinn Ft. Chilly',
+    artwork: require('./assets/Musics/phocuconanh/phocuconanh.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/3107/3107.mp3'),
+    title: '3107',
+    artist: 'W/N ft. Nâu, Duongg',
+    artwork: require('./assets/Musics/3107/3107.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/3107-2/3107-2.mp3'),
+    title: '3107-2',
+    artist: 'W/N ft. Nâu, Duongg',
+    artwork: require('./assets/Musics/3107-2/3107-2.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/3107-3/3107-3.mp3'),
+    title: '3107-3',
+    artist: 'W/N ft. 267, Nâu, Duongg',
+    artwork: require('./assets/Musics/3107-3/3107-3.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/31077/31077.mp3'),
+    title: '31077',
+    artist: 'W/N ft. 267, Nâu, Duongg',
+    artwork: require('./assets/Musics/31077/31077.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/loinho/loinho.mp3'),
+    title: 'Lối nhỏ',
+    artist: 'Đen ft. Phương Anh Đào',
+    artwork: require('./assets/Musics/loinho/loinho.jpg'), // Load artwork from the network  
+  },
+  {
+    url: require('./assets/Musics/divenha/divenha.mp3'),
+    title: 'Đi về nhà',
+    artist: 'Đen x Justatee',
+    artwork: require('./assets/Musics/divenha/divenha.jpg'), // Load artwork from the network  
+  },
+];
+
+export default tracks;
